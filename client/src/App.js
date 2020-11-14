708eb6fc7bfabcc14e3bb7f7206b305c2f9a182e
Brian <brian.holland.pr@gmail.com>
2020-11-14 01:04:01 -0800

    register page completed

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
 import './App.css';
 
 import ApplicationState from './context/application/ApplicationState';
@@ -19,6 +20,7 @@ const App = () => {
 							<Switch>
 								<Route exact path="/" component={Home} />
 								<Route exact path="/about" component={About} />
+								<Route exact path="/register" component={Register} />
 							</Switch>
 						</div>
 					</Fragment>
